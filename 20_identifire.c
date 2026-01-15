f91744a2b4072be2349379293e817a24f58b6899
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2026-01-15 16:57:24 +0530

    01/12

---
+++ 20_identifire.c
@@ -0,0 +1,9 @@
+//example of identifire
+#include<stdio.h>
+void main()
+{
+    int _4age12;
+    // int while;// show error
+    int age;
+    int Age;
+}
---
+++ 21_constant.c
@@ -0,0 +1,8 @@
+// constant variable eaxmple
+#include <stdio.h>
+void main()
+{
+    const int age = 12;
+    // age = 90; // its show error
+    printf("age = %d", age);
+}
