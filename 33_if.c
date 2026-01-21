3f5ff453eea619ec9d3ebe1b6b59ec9ab1402ed5
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2026-01-21 16:10:49 +0530

    01/12

---
+++ 33_if.c
@@ -0,0 +1,12 @@
+// if example
+#include <stdio.h>
+void main()
+{
+    printf("this is stmnt1\n");
+    if (12 > 77)
+    {
+        printf("this is stmnt2\n");
+        printf("this is stmnt3\n");
+    }
+    printf("this is stmnt4\n");
+}
---
+++ 34_ifElse.c
@@ -0,0 +1,17 @@
+// example of if-else
+#include <stdio.h>
+void main()
+{
+    printf("this is stmnt 1\n");
+    if (12 > 77)
+    {
+        printf("this is stmnt 2\n");
+        printf("this is stmnt 3\n");
+    }
+    else
+    {
+        printf("this is stmnt 4\n");
+        printf("this is stmnt 5\n");
+    }
+    printf("this is stmnt 6\n");
+}
